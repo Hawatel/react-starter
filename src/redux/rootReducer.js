fba0eceeb94b5ed79df87743f0a82f70da0bb558
Daniel Iwaniuk <daniel.iwaniuk@hawatel.com>
2016-04-20 11:21:13 +0200

    Add Rest test method

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { items } from './modules/items';
+import { getGeometryReducer } from './modules/apilocation';
 
 const rootReducer = combineReducers({
   form: formReducer,
   /* your reducers */
   items,
+  getGeometryReducer
 });
 
 export default rootReducer;
