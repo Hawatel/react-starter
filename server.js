a974ab02e8615cfba2070bf61959e8d4e76c2349
Przemyslaw Mantaj <przemyslaw.mantaj@hawatel.com>
2016-05-08 18:14:01 +0200

    Added localhost API for testing purposes
    * a path to the api for payments: /api/payment, only post method is acceptable with fields: number, cvc, year, month. Look at the api/payment.json file to see server response examples.
    * a path to the api for coupons: /api/coupon, only post method is acceptable with fields: coupon. Look at the api/coupon.json file to see server response examples.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+const fs = require("fs");
 
 app.use(require('morgan')('short'));
+
+var jsonParser = bodyParser.json();
 
 (function initWebpack() {
   const webpack = require('webpack');
@@ -19,6 +23,38 @@ app.use(require('morgan')('short'));
 
   app.use(express.static(__dirname + '/'));
 })();
+
+app.post('/api/payment/', jsonParser, function(req, res) {
+  setTimeout(function() {
+    fs.readFile(__dirname + "/" + "api/payment.json", 'utf8', function(err, data) {
+      let obj = JSON.parse(data);
+      console.log(req.body);
+      for(var i=0; i < obj.length; i++) {
+        if(obj[i].number == req.body.number &&
+            obj[i].cvc == req.body.cvc &&
+            obj[i].expiration.year == req.body.year &&
+            obj[i].expiration.month == req.body.month) {
+          res.end('{"result": "valid"}');
+        }
+      }
+      res.end('{"result": "not valid"}');
+    })
+  }, 4000);
+});
+
+app.post('/api/coupon', jsonParser, function(req, res) {
+  setTimeout(function() {
+    fs.readFile(__dirname + "/" + "api/coupon.json", 'utf8', function(err, data) {
+      let obj = JSON.parse(data);
+      for(var i=0; i < obj.length; i++) {
+        if(obj[i] == req.body.coupon) {
+          res.end('{"result": "valid"}');
+        }
+      }
+      res.end('{"result": "not valid"}');
+    })
+  }, 4000); // simulate server latency
+});
 
 app.get(/.*/, function root(req, res) {
   res.sendFile(__dirname + '/index.html');
